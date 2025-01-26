3de67b9ce02f0ece713484b7eb0493016613138d
omar <omarsameh20017@gmail.com>
2025-01-26 19:39:22 +0200

    import in main file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const userBookRoutes = require('./routes/userBookRoutes');
 
 const app = express();
 app.use(cors());
@@ -11,6 +11,10 @@ mongoose.connect('mongodb://localhost:27017/insurance-system', {
   
 }).then(() => console.log('Connected to MongoDB'))
   .catch(error => console.error('Could not connect to MongoDB', error));
+
+
+// UserBooks routes
+app.use(userBookRoutes);
 
 const port = 3001;
 app.listen(port, () => {
