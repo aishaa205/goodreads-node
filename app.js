574be4917cafd52704ac69d92b52df66683b6db1
aishaa205 <aishaamr54@gmail.com>
2025-01-27 17:13:45 +0200

    g

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://enghusseinsaad1:cCxlORhhU68ziVmn@cluster0.6auvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  
 }).then(() => console.log('Connected to MongoDB'))
   .catch(error => console.error('Could not connect to MongoDB', error));
+app.use('/categories', categoryRoutes);
 
-  app.use('/categories', categoryRoutes);
- 
-  app.use("/books",bookRoutes);
-  app.use("/authors", authorRoutes);
+app.use("/books",bookRoutes);
+app.use("/authors", authorRoutes);
 
 
 const port = 3001;
