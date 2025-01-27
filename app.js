0a9be7d1fafe8ab464820facf69fcefab726f9b5
Hoda Magdy <hmag425@gmail.com>
2025-01-27 15:44:54 +0200

    connect to mongo server

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const categoryRoutes = require('./routes/categoryRoutes');  
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/insurance-system', {
+mongoose.connect('mongodb+srv://enghusseinsaad1:cCxlORhhU68ziVmn@cluster0.6auvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   
 }).then(() => console.log('Connected to MongoDB'))
   .catch(error => console.error('Could not connect to MongoDB', error));
-
+app.use('/categories', categoryRoutes);
 const port = 3001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
