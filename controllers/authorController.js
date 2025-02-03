48b5c94e48b965cdf1bb3bf40dc084f34ed1c50a
aishaa205 <aishaamr54@gmail.com>
2025-02-03 10:37:30 +0200

    fix controllers

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -4,10 +4,16 @@ const Author = require("../models/author");
 
 exports.createAuthor = async (req, res) => {
     try {
+
+      const { name } = req.body;
+      const existingAuthor = await Author.findOne({ name });
+      if (existingAuthor) {
+        return res.status(400).json({ message: "Author already exists" });
+      }
       const author = new Author(req.body);
       await author.save();
-      res.status(201).send(author);
-      // console.log("hello")
+      res.status(201).json({ success: true, author });
+      
     } catch (error) {
       res.status(400).send(error);
     }
@@ -18,7 +24,7 @@ exports.createAuthor = async (req, res) => {
 
   exports.getAuthors = async (req, res) => {
     try {
-      const authors = await Author.find().populate("books");
+      const authors = await Author.find().populate("books",'name');
       res.status(200).send(authors);
     } catch (error) {
       res.status(500).send(error);
--- controllers/bookController.js
+++ controllers/bookController.js
@@ -8,10 +8,15 @@ const mongoose = require("mongoose");
 
 exports.createBook = async (req, res) => {
   try {
+    const existingBook = await Book.findOne({ title, author, edition });
+
+    if (existingBook) {
+      return res.status(400).json({ success: false, message: "Book already exists." });
+    }
     const book = new Book(req.body);
     await book.save();
 
-    res.status(201).send(book);
+    res.status(201).json({ success: true, book });
   } catch (error) {
     res.status(400).send(error);
   }
@@ -64,101 +69,7 @@ exports.getBook = async (req, res) => {
     }
   };
 
-// filter matense4
 
-// exports.getBooksfilter = async (req, res) => {
-//   try {
-//     const { categories, authors } = req.query;
-//     let filter = {};
-
-//     // Process category filter
-//     if (categories) {
-//       const categoryIds = categories.split(",")//.map(id => mongoose.Types.ObjectId(id.trim()));
-//       filter.category = { $in: categoryIds };
-//     }
-
-//     // Process author filter
-//     if (authors) {
-//       const authorIds = authors.split(",")//.map(id => mongoose.Types.ObjectId(id.trim()));
-//       filter.author = { $in: authorIds };
-//     }
-
-//     // Query the books with filters applied
-//     const books = await Book.find(filter)
-//       //.populate("author") // Populating author details
-//       //.populate("category"); // Populating category details
-
-//     if (books.length > 0) {
-//       return res.status(200).json({ success: true, books });
-//     } else {
-//       return res.status(404).json({ success: false, message: "No books found with the given filters." });
-//     }
-//   } catch (error) {
-//     console.error("Error:", error);
-//     return res.status(500).json({ success: false, message: "Server error." });
-//   }
-// };
-
-/////////////////////////////////////
-
-
-// exports.getBooksfilter = async (req, res) => {
-//   try {
-//     const { categories, authors } = req.query;
-//     let filter = {};
-
-//     let categoryIds = [];
-//     let authorIds = [];
-
-//     // Validate category IDs by checking their existence in the Book collection
-//     if (categories) {
-//       categoryIds = categories.split(",")//.map(id => id.trim());
-//        console.log(categoryIds);
-//       // Find unique category IDs that exist in the Book collection
-//       const existingCategories = await Book.distinct("category", { category: { $in: categoryIds } });
-//       console.log(existingCategories);
-//       if (existingCategories.length !== categoryIds.length) {
-//         return res.status(400).json({ success: false, message: "One or more category IDs do not exist in any book." });
-//       }
-
-//       filter.category = { $in: existingCategories };
-//     }
-
-//     // Validate author IDs by checking their existence in the Book collection
-//     if (authors) {
-//       authorIds = authors.split(",");//.map(id => id.trim());
-// console.log(authorIds);
-//       // Find unique author IDs that exist in the Book collection
-//       const existingAuthors = await Book.distinct("author", { author: { $in: authorIds } });
-//        console.log(existingAuthors);
-//       if (existingAuthors.length !== authorIds.length) {
-//         return res.status(400).json({ success: false, message: "One or more author IDs do not exist in any book." });
-//       }
-
-//       filter.author = { $in: existingAuthors };
-//     }
-
-//     // Query the books with the validated filters
-//     const books = await Book.find(filter)
-//       .populate("author") // Populate author details
-//       .populate("category"); // Populate category details
-
-//     if (books.length > 0) {
-//       return res.status(200).json({ success: true, books });
-//     } else {
-//       return res.status(404).json({ success: false, message: "No books found with the given filters." });
-//     }
-//   } catch (error) {
-//     console.error("Error:", error);
-//     return res.status(500).json({ success: false, message: "Server error." });
-//   }
-// };
-
-
-
-
-//////////////////////////////////////////
-//da a7san aw7da fehom
 exports.getBooksfilter = async (req, res) => {
   try {
     const { categories, authors } = req.query;
@@ -192,8 +103,8 @@ exports.getBooksfilter = async (req, res) => {
     }
 
     const books = await Book.find({ $or: filter })
-      .populate("author")
-      .populate("category");
+    .populate("author", "name") 
+    .populate("category", "name");
 
     if (books.length > 0) {
       return res.status(200).json({ success: true, books });
