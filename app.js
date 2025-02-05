2f01ce3725b18a401003f778ac83c9713265e3c0
aishaa205 <aishaamr54@gmail.com>
2025-02-05 16:15:08 +0200

    add googleapi and multer

--- app.js
+++ app.js
@@ -14,9 +14,147 @@ const routes = require("./routes");
 const app = express();
 const path = require("path");
 
+
+  
+//////////////////////////
+// const fs = require("fs");
+// const {google} = require("googleapis");
+// const multer = require("multer");
+// // access json file
+// const apikey = require("./apikey.json");
+
+// const SCOPE = ['https://www.googleapis.com/auth/drive'];
+// // Authorization to google drive 
+// async function authorize(){
+//    const jwtClient = new  google.auth.JWT(
+//      apikey.client_email,
+//      null ,
+//      apikey.private_key,
+//      SCOPE
+//    );
+//   await jwtClient.authorize();
+//   return jwtClient;
+
+// } 
+
+///////////////da el 2deem
+// //upload file to google drive 
+// async function uploadFile(authClient){
+//   return new Promise((resolve,rejected)=>{
+//       const drive = google.drive({version:'v3',auth:authClient}); 
+
+//       // Validate file format
+//       if (path.extname('./test.pdf').toLowerCase() !== '.pdf') {
+//         return reject(new Error('Only PDF files are allowed!'));
+//       }
+
+//       // accept ay file m4 pdf bs
+//       var fileMetaData = {
+//           name:'',    
+//           parents:['16sqCCx5yT5Lrm4TJAcDeXG4RuPJHP-Xv'] //el folder id mn el url bta3 drive
+//       }
+//       drive.files.create({
+//           resource:fileMetaData,
+//           media:{
+//               body: fs.createReadStream('./test.pdf'), // files that will get uploaded
+//               mimeType: 'application/pdf'
+//           },
+//           fields:'id'
+//       },function(error,file){
+//           if(error){
+//               return rejected(error)
+//           }
+//           resolve(file);
+//       })
+//   });
+// }
+// authorize().then(uploadFile).catch("error",console.error());
+
+
+///////da el gded
+
+// async function uploadFile(authClient, filePath, fileName) {
+//   const drive = google.drive({ version: "v3", auth: authClient });
+
+//   const fileMetaData = {
+//       name: fileName,
+//       parents: ["16sqCCx5yT5Lrm4TJAcDeXG4RuPJHP-Xv"], 
+//   };
+
+//   const media = {
+//       mimeType: "application/pdf",
+//       body: fs.createReadStream(filePath),
+//   };
+
+//   try {
+//       const response = await drive.files.create({
+//           resource: fileMetaData,
+//           media: media,
+//           fields: "id",
+//       });
+
+//       // Construct file URL
+//       const fileId = response.data.id;
+//       const fileUrl = `https://drive.google.com/uc?id=${fileId}`;
+
+//       // Delete file from local storage after uploading
+//       fs.unlinkSync(filePath);
+
+//       return fileUrl;
+//   } catch (error) {
+//       console.error("Error uploading file:", error);
+//       throw error;
+//   }
+// }
+
+
+
+// app.post("/upload-files", upload.single("pdf"), async (req, res) => {
+//   try {
+//       if (!req.file) {
+//           return res.status(400).json({ message: "No file uploaded!" });
+//       }
+
+//       const authClient = await authorize();
+//       const fileUrl = await uploadFile(authClient, req.file.path, req.file.originalname);
+
+//       return res.status(200).json({ message: "File uploaded successfully!", fileUrl });
+//   } catch (error) {
+//       res.status(500).json({ message: "Error uploading file", error });
+//   }
+// });
+
+
+// app.listen(3001, () => {
+//   console.log("Server is running on port 3001");
+// });
+
+
+
+/////////
+// const multer = require("multer");
+
+
+// //storege b destination mo3ayan w link
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//       cb(null, "./files");
+//   }, 
+//   filename: function (req, file, cb) {
+//        const uniqueSuffix = Date.now() 
+//        cb(null, uniqueSuffix+file.originalname)
+//   },
+// });
+// const upload = multer({ storage: storage })
+
+// app.post("/upload-files",upload.single("file"),async(req,res)=>{
+//   console.log(req.file);
+//   })
+
+
 //const passport = require("passport");
 //require("./config/passport");
-const authRoutes = require("./routes/auth");
+// const authRoutes = require("./routes/auth");
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
